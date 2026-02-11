a7cb9cd5cec8e15dd9c28e078e72397b71adb97b
AnneRom <coolann000.com@gmail.com>
2026-02-11 17:32:09 +0200

    11.02
    Файли слайсів. Функція createSlice. Оновлення стану з Immer.

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,41 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
+import tasksReducer from "./tasksSlice";
+import filtersReducer from "./filtersSlice";
 
-const initialState = {
-    tasks: {
-	    items: [
-	    { id: 0, text: "Learn HTML and CSS", completed: true },
-	    { id: 1, text: "Get good at JavaScript", completed: true },
-	    { id: 2, text: "Master React", completed: false },
-	    { id: 3, text: "Discover Redux", completed: false },
-	    { id: 4, text: "Build amazing apps", completed: false },
-	  ],
-    },
-    filters: {
-        status: "all",
-  },
-};
-
-
-
-const filtersReducer = (state = initialState.filters, action) => {
-  switch (action.type) {
-    
-    case 'filters/setStatusFilter':
-      return {
-        ...state,
-        status: action.payload,  
-      }
-    
-    default:
-    return state;
-  }
-  
-
-
-}
-
-const rootReducer = (state = initialState, action) => {
-}
+// const rootReducer = (state = initialState, action) => {
+// }
 
 export const store = configureStore({
     reducer: {
--- src/redux/tasksSlice.js
+++ src/redux/tasksSlice.js
@@ -1,42 +1,35 @@
-const initialState = {
-    tasks: {
-	    items: [
-	    { id: 0, text: "Learn HTML and CSS", completed: true },
-	    { id: 1, text: "Get good at JavaScript", completed: true },
-	    { id: 2, text: "Master React", completed: false },
-	    { id: 3, text: "Discover Redux", completed: false },
-	    { id: 4, text: "Build amazing apps", completed: false },
-	  ],
-    }
-};
+import { createSlice } from "@reduxjs/toolkit";
 
-export default function tasksReducer (state = initialState.tasks, action) {
-  switch (action.type) {
-    case 'tasks/addTask':
-      return {
-        ...state,
-        items: [...state.items, action.payload]
-      }
-    case 'tasks/deleteTask':
-      return {
-        ...state,
-        items: state.items.filter(task => task.id !== action.payload)
-      }
-    case 'tasks/toggleTask':
-      return {
-        ...state,
-          items: state.items.map(task => {
-            if (task.id === action.payload) {
-              return {
-                ...task,
-              completed: !task.completed,
-            };
-          }
-          return task;
-       
-        })
+const slice = createSlice({
+    name: 'tasks',
+    initialState: {
+        items: [
+            { id: 0, text: "Learn HTML and CSS", completed: true },
+            { id: 1, text: "Get good at JavaScript", completed: true },
+            { id: 2, text: "Master React", completed: false },
+            { id: 3, text: "Discover Redux", completed: false },
+            { id: 4, text: "Build amazing apps", completed: false },
+        ], 
+
+    },
+    reducers: {
+        addTask: (state, action) => {
+                state.items.push(action.payload)
+        },
+        deleteTask: (state, action) => {
+                state.items = state.items.filter(task => task.id !== action.payload)
+        },
+        toggleTask: (state, action) => {
+            for (const task of state.items) {
+                if (task.id === action.payload) {
+                    task.completed = !task.completed;
+                    break;
+                }
+            }
+        }
+
     }
-    default:
-    return state;
-  }
-}
+
+})
+export const { addTask, deleteTask, toggleTask } = slice.actions;
+export default slice.reducer;
